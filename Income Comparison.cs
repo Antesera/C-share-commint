dcaa0e85ee8e5b460a1dae8fe084695b84e2fa63
Antesera <107664612+Antesera@users.noreply.github.com>
2022-10-01 12:46:35 -0400

    Add files via upload

---
+++ Income Comparison.cs
@@ -0,0 +1,52 @@
+using Systems;
+using System.collections.Generics;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+ namespace MathAndcomparisonoperators
+  {
+     Class MainClass
+	  {
+	     Public static void Main(string[] args)
+		   {
+		   int Hour Rate persone1 = 25;
+		   Console.WriteLine (Hour Rate persone1);
+		   Consile.ReadLine ();
+		   
+		    product Daily rate = 8 * 25;
+            Console.WriteLine(product);
+            Console.ReadLine();
+			
+			product Weekly rate =  40 * 25 ;
+            Console.WriteLine(product);
+            Console.ReadLine();
+			
+			product Annualy rate = 1000 * 52 ;
+            Console.WriteLine(product);
+            Console.ReadLine();
+			
+			int Hour Rate persone2 = 20;
+		   Console.WriteLine (Hour Rate persone1);
+		   Consile.ReadLine ();
+		   
+		    product Daily rate = 8 * 20;
+            Console.WriteLine(product);
+            Console.ReadLine();
+			
+			product Weekly rate =  40 * 20 ;
+            Console.WriteLine(product);
+            Console.ReadLine();
+			
+			product Annualy rate = 800 * 52 ;
+            Console.WriteLine(product);
+            Console.ReadLine();
+			
+			//bool trueOrfalse = 800* 52 < 1000 * 52;
+        //Console.WriteLine(trueOrfalse.ToString());
+        //Console.ReadLine();
+			
+		   
+		   }
+	  }
+  }
